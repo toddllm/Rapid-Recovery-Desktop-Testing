199eb8c89d2ce3cc78516fd0bccc0cfa197fbefe
Todd Deshane <deshantm@gmail.com>
2010-06-20 15:21:52 -0400

    add run_iozone script, re-arrange results

---
+++ run_iozone.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+platform=linux-base
+to=linux-base
+num_trials=3
+results_dir='/home/deshantm/Rapid-Recovery-Desktop-Testing/results'
+max_file_size=1024
+iozone_dir=/home/deshantm/iozone3_347/src/current
+cd $iozone_dir
+
+num=1
+while [ $num -le $num_trials ]; do
+  (time ./iozone -i0 -i1 -a -g $max_file_size > ${results_dir}/iozone-${platform}-to-${to}-trial_${num}.out) 2>> ${results_dir}/iozone-${platform}-to-$to-timelog.out
+  num=$(( $num + 1 ))
+done
+
