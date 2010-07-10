4507d773a01de55decfdd75b5efecc72baccc8b7
Todd Deshane <deshantm@gmail.com>
2010-07-10 17:51:47 -0400

    fix netipe setup script

--- setup-netpipe.sh
+++ setup-netpipe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+cd ~/
 wget http://www.scl.ameslab.gov/netpipe/code/NetPIPE-3.7.1.tar.gz
 tar xf NetPIPE-3.7.1.tar.gz
-cd NetPIPE-3.7.1
+cd ~/NetPIPE-3.7.1
 make
 echo 'now you can run the NPtcp binary inside of the NetPIPE-3.7.1 directory' 
